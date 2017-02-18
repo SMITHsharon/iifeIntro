76d5ae9432b8fbe1b46a0e46433604ca8b59d569
sharon smith <sharon.e.smith2@gmail.com>
2017-02-18 11:13:45 -0600

    most working, except for planets

--- javascripts/solarsystem.js
+++ javascripts/solarsystem.js
@@ -1,38 +1,15 @@
 var SolarSystem = (function(){
 
-	var planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"];
-	var numPlanetsPeopleLandedOn = 0;
-	var stars = ["Sun", "Alpha Centauri", "Wolf 359"];
-	var age = 0;
+	
 
 	return {
-		getPlanets: function(){
-			return planets;
-		},
-		getPlanetsLandedPeopleOn: function(){
-			return numPlanetsPeopleLandedOn;
-		},
-		setPlanetsLandedPeopleOn: function(){
-			numPlanetsPeopleLandedOn++;
-		},
+		
 		getDwarfPlanets: function(){
 			return "they are rejects";
 		},
 		setDwarfPlanets: function(lumpOfRock){
 			dwarfPlanets.push(lumpOfRock);
-		},
-		getStars: function(){
-			return stars;
-		},
-		setStars: function(){
-			stars.push(newStar);
-		},
-		getSolarSystemAge: function(){
-			return age;
-		},
-		setSolarSystemAge: function(){
-			age++;
-		},
+		}
 	}
 
 })();
